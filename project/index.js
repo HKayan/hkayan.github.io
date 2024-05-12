d4d28021cc119d135f69c8dc3cf4a6f56b764f75
Hasan Kayan <hasankayan.73@gmail.com>
2024-05-12 23:41:00 +0300

    rateyo plugin

--- project/index.js
+++ project/index.js
@@ -38,36 +38,54 @@ $(document).ready(function() {
     }
 
     // Load and display content from data.json
-    $.getJSON('data.json', function(data) {
-        // Populate language learning container
-        $.each(data.LanguageLearning, function(key, val) {
-            var item = '<div class="lan-learn-blog"><h3>' + val.title + '</h3><img class="blog-img" src="' + val.image + '" alt="' + val.title + '"><div class="content"><p class="shortText">' + val.content.substring(0, 100) + '...</p><p class="fullText" style="display:none;">' + val.content + '</p><button class="toggleButton">Read More/Less</button></div>';
-            $('#language-learning-container').append(item);
-        });
+    $(document).ready(function() {
 
-        // Handle Learning Environment data
-        $.each(data.LearningEnvironment, function(key, val) {
-            var item = '<div class="lear-env-blog"><h3>' + val.title + '</h3><img class="blog-img" src="' + val.image + '" alt="' + val.title + '"><div class="content"><p class="shortText">' + val.content.substring(0, 100) + '...</p><p class="fullText" style="display:none;">' + val.content + '</p><button class="toggleButton">Read More/Less</button></div>';
-            $('#learning-environment-container').append(item);
-        });
+        function createBlogItem(val, containerId, blogClass) {
+            var item = `<div class="${blogClass}"><h3>${val.title}</h3><img class="blog-img" src="${val.image}" alt="${val.title}"><div class="content"><p class="shortText">${val.content.substring(0, 100)}...</p><p class="fullText" style="display:none;">${val.content}</p><button class="toggleButton">Read More/Less</button><div class='rating'></div></div></div>`;
+            $(containerId).append(item);
+        }
+        
 
-        // Handle Personal Reflections data
-        $.each(data.PersonalReflections, function(key, val) {
-            var item = '<div class="pers-ref-blog"><h3>' + val.title + '</h3><img class="blog-img" src="' + val.image + '" alt="' + val.title + '"><div class="content"><p class="shortText">' + val.content.substring(0, 100) + '...</p><p class="fullText" style="display:none;">' + val.content + '</p><button class="toggleButton">Read More/Less</button></div>';
-            $('#personal-reflections-container').append(item);
-        });
+        function initiateRatings() {
+            $('.rating').rateYo({
+                fullStar: true,
+                onSet: function(rating, rateYoInstance) {
+                    console.log(`Rating set to ${rating}`);
+                }
+            });
+        }
 
+        $.getJSON('data.json', function(data) {
 
-        $('.toggleButton').on('click', function() {
-            var fullText = $(this).siblings('.fullText');
-            var shortText = $(this).siblings('.shortText');
-            if (fullText.css('display') === 'none') {
-                fullText.css('display', 'block');
-                shortText.css('display', 'none');
-            } else {
-                fullText.css('display', 'none');
-                shortText.css('display', 'block');
+            
+    
+            // Process and display each category of data
+            function processCategory(category, containerId,blogClass) {
+                $.each(data[category], function(key, val) {
+                    createBlogItem(val, containerId, blogClass);
+                });
             }
+    
+            // Process each category
+            processCategory('LanguageLearning', '#language-learning-container','lan-learn-blog');
+            processCategory('LearningEnvironment', '#learning-environment-container', 'lear-env-blog');
+            processCategory('PersonalReflections', '#personal-reflections-container', 'pers-ref-blog');
+
+            initiateRatings();
+    
+            // Toggle button functionality
+            $('.toggleButton').on('click', function() {
+                var fullText = $(this).siblings('.fullText');
+                var shortText = $(this).siblings('.shortText');
+                if (fullText.css('display') === 'none') {
+                    fullText.css('display', 'block');
+                    shortText.css('display', 'none');
+                } else {
+                    fullText.css('display', 'none');
+                    shortText.css('display', 'block');
+                }
+            });
         });
     });
+    
 });
